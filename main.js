370717f8c728a21ad2d07d58e3d37099d6575896
KaterynaTishchenko <111631799+KaterynaTishchenko@users.noreply.github.com>
2023-04-25 21:03:14 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    $('.slider-inner').slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+
+});
